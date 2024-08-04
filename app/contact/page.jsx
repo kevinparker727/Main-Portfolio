229eb591e4e0c6f1cdfe64945b41790244e9a01b
kevinparker727 <kevin.parker.727@gmail.com>
2024-08-04 14:16:01 -0400

    Added form submit logic

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -29,6 +30,26 @@ const info = [
 import { motion } from "framer-motion";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_22cu3cc", "template_wwmpguv", form.current, {
+        publicKey: "LdZ4w0Zji_2cYZMie",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+          e.target.reset();
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
+
   return (
     <motion.section
       initial={{ opacity: 0 }}
@@ -39,24 +60,38 @@ const Contact = () => {
       className="py-6"
     >
       <div className="container mx-auto">
-        <div className="flex flex-col xl:flex-row gap-[30px]">
+        <div className="flex flex-col xl:flex-row xl:justify-around gap-[30px]">
           {/* form  */}
           <div className="xl:w-[54%] order-2 xl:order-none">
-            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+            <form
+              ref={form}
+              onSubmit={sendEmail}
+              className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl"
+            >
               <h3 className="text-4xl text-accent">
                 Let&apos;s work together!
               </h3>
               <p className="text-white/60">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
-                eius qui animi sunt non!
+                Thanks for reaching out! I'm currently accepting job offers for
+                both in-office, hybrid, and fully remote work and am willing to
+                relocate. Shoot me a message, and let's talk about your project!
               </p>
               {/* input  */}
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                <Input type="firstname" placeholder="First name" />
-                <Input type="lastname" placeholder="Last name" />
+                <Input
+                  type="firstname"
+                  placeholder="First name"
+                  name="first_name"
+                />
+                <Input
+                  type="lastname"
+                  placeholder="Last name"
+                  name="last_name"
+                />
                 <Input
                   type="email"
                   placeholder="Email"
+                  name="email"
                   className="md:col-span-2"
                 />
               </div>
@@ -65,10 +100,11 @@ const Contact = () => {
               <Textarea
                 className="h-[200px]"
                 placeholder="Type your message here."
+                name="message"
               />
 
               {/* button  */}
-              <Button size="md" className="max-w-40">
+              <Button type="submit" value="send" size="md" className="max-w-40">
                 Send Message
               </Button>
             </form>
